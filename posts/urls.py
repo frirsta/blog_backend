7176dff17a18793f91176f6ab1ad8b333ac614e6
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-07 22:43:39 +0200

    Fix PostDetail url

--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,6 @@ from .views import PostListView, PostDetail
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post-detail'),
-    path('<int:id>/', PostDetail.as_view(), name='post-detail-other'),
+    path('', PostListView.as_view(), name='post-list'),
+    path('<int:id>/', PostDetail.as_view(), name='post-detail'),
 ]
