644e74a80825f03727314d960bbd0f4e2a3ad41d
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-11 07:34:00 +0200

    Update PostSerializer to include owner's profile picture.

--- posts/serializers.py
+++ posts/serializers.py
@@ -10,6 +10,13 @@ class PostSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         return obj.author == request.user
 
+    def get_profile_picture(self, obj):
+        try:
+            return obj.author.profile.profile_picture.url
+        except AttributeError:
+            return None
+
     class Meta:
         model = Post
-        fields = '__all__'
+        fields = ['id', 'title', 'content', 'owner', 'is_owner',
+                  'profile_picture', 'image', 'created_at', 'updated_at', 'author']
