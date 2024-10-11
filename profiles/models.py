024f8a3808c60701345d03629d6d9d4fe7471066
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-11 03:48:20 +0200

    Add default profile picture

--- profiles/models.py
+++ profiles/models.py
@@ -6,7 +6,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(
-        upload_to='profile_pictures/', blank=True, null=True)
+        upload_to='profile_pictures/', blank=True, null=True, default='profile_pictures/default.png')
     location = models.CharField(max_length=100, blank=True, null=True)
     website = models.URLField(blank=True, null=True)
 
