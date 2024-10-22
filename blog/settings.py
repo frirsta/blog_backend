1cb329a45f161d298315477c30d7a1536b7437ba
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-22 23:38:59 +0200

    Add password reset request endpoint. Configure email backend to use Brevo SMTP for password reset emails. Update profiles app URLs to include password reset routes.

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 import cloudinary
 import cloudinary.uploader
@@ -102,7 +103,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -170,3 +171,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp-relay.brevo.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from .views import ProfileDetailView, AccountDeleteView, UserRegistrationView
 
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='user-registration'),
-    path('', ProfileDetailView.as_view(), name='profile-detail'),  # logged-in user
-    path('<int:user_id>/', ProfileDetailView.as_view(), name='profile-detail-other'),
+    path('', ProfileDetailView.as_view(),
+         name='profile-detail'),  # logged-in user
+    path('<int:user_id>/', ProfileDetailView.as_view(),
+         name='profile-detail-other'),
     path('delete/', AccountDeleteView.as_view(), name='account-delete'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
