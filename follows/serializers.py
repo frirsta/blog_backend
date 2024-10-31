169784efca837ad98eaa37af60928c3058cae8cf
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-31 16:49:56 +0100

    Update FollowSerializer

--- follows/serializers.py
+++ follows/serializers.py
@@ -3,7 +3,8 @@ from .models import Follow
 
 
 class FollowSerializer(serializers.ModelSerializer):
+    follower = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Follow
         fields = '__all__'
-
