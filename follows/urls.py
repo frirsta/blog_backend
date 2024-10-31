5e61a6ccc00ae80ca438670a06aec360b2f898be
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-10-31 17:01:04 +0100

    Update the urlpatterns for the follows app

--- follows/urls.py
+++ follows/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import FollowDetail, FollowListView
 
 urlpatterns = [
-    path('<int:user_id>/', FollowDetail.as_view(), name='follow-user'),
+    path('<int:pk>/', FollowDetail.as_view(), name='follow-user'),
     path('', FollowListView.as_view(), name='follow-list'),
 ]
