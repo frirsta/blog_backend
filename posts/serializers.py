cfdf51dc2945ad662c606bcf0191eaac77531913
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-11-07 07:59:08 +0100

    Update tag to not required

--- posts/serializers.py
+++ posts/serializers.py
@@ -28,7 +28,8 @@ class PostSerializer(serializers.ModelSerializer):
         queryset=Category.objects.all(), many=True, write_only=True, source='category')
     tags = TagSerializer(many=True, read_only=True)
     tags_names = serializers.ListField(
-        child=serializers.CharField(), write_only=True)
+        child=serializers.CharField(), write_only=True, required=False
+    )
 
     class Meta:
         model = Post
@@ -75,19 +76,23 @@ class PostSerializer(serializers.ModelSerializer):
         return value
 
     def create(self, validated_data):
-        tags_names = validated_data.pop('tags_names', [])
+        tags_data = validated_data.pop('tags_names', [])
         post = super().create(validated_data)
-        for tag_name in tags_names:
+
+        for tag_name in tags_data:
             tag, created = Tag.objects.get_or_create(name=tag_name)
             post.tags.add(tag)
+
         return post
 
     def update(self, instance, validated_data):
-        tags_names = validated_data.pop('tags_names', [])
+        tags_data = validated_data.pop('tags_names', None)
         instance = super().update(instance, validated_data)
-        if tags_names:
+
+        if tags_data is not None:
             instance.tags.clear()
-            for tag_name in tags_names:
+            for tag_name in tags_data:
                 tag, created = Tag.objects.get_or_create(name=tag_name)
                 instance.tags.add(tag)
+
         return instance
